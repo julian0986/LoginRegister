6716239cd23d27f9c3a2ac80fe093235ca9ac706
Julian Calle <julian0986@gmail.com>
2022-12-21 16:06:10 -0500

    se coloca video

--- php/bienvenido.php
+++ php/bienvenido.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 session_start();
 
-if(!isset($_SESSION['usuario'])){
+if (!isset($_SESSION['usuario'])) {
     echo '
         <script>
             alert("Porfavor debes iniciar sesion");
@@ -17,14 +17,18 @@ if(!isset($_SESSION['usuario'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido</title>
 </head>
+
 <body>
     <h1>hola ya ingrese</h1>
     <a href="./cerrar_sesion.php">Cerrar Sesion</a>
+    <iframe width="1161" height="653" src="https://www.youtube.com/embed/TzYFOMjECDQ" title="la reacción de mbappe cuando " Dibu" intenta de consolarlo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </body>
+
 </html>
