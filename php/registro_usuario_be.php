44641bb43566b4e6f03974f18371a594d77d1d43
Julian Calle <julian0986@gmail.com>
2022-12-21 11:42:07 -0500

    Creando la encriptacion de la ocntraseña

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -4,6 +4,7 @@ $nombre_completo = $_POST['nombre_completo'];
 $correo = $_POST['correo'];
 $usuario = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
+$contrasena =hash('sha512',$contrasena);
 
 $query = "INSERT INTO usuarios (nombre_completo,correo,usuario,contrasena) 
           VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena')";
