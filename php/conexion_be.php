d03612012bfb4129572c9f4a037809aa9a95c8e3
julian0986 <julian0986@gmail.com>
2023-01-09 22:38:19 -0500

    despliegue en heroku

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,14 +1,15 @@
 <?php
 
-$conexion = mysqli_connect("localhost","root","","login_register");
+//$conexion = mysqli_connect("localhost","root","","login_register");
+$conexion = mysqli_connect("www.db4free.net","login_julian","Calle1128*","log_register");
 
 //prueba de conexion.
-/* if($conexion){
-    echo "conexion exitosa a la base de datos";
+ if($conexion){
+    echo "conexion externa exitosa a la base de datos";
 }else{
     echo "No se puede conectar a la base de datos";
 }
- */
+ 
 
 
 ?>
